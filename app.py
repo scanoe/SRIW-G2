5c634a07cc08b0f8f26d36264a0a1bbe99b2cc4c
Daniel Cano <dacanoc@unal.edu.co>
2018-05-31 15:14:31 -0500

    Agregado caso en que usuario no tiene calificaciones

--- app.py
+++ app.py
@@ -144,217 +144,219 @@ def recomendar():
     cursor.execute("SELECT idips, `like` FROM calificaciones WHERE email='" + email + "'")
     data = cursor.fetchall()
     conn.close()
-    lista = []
-    for d in data:
-        idips = d[0]
-        like = d[1]
-        query ='PREFIX ips:<http://www.EPSColombia.org#>\
-        SELECT *\
-        WHERE {\
-            ?ips ips:idips ?id;\
-            ips:nomservicio ?ser;\
-            ips:municipio ?mun;\
-            ips:departamento ?dep\
-            FILTER REGEX(?id, "' + idips + '+")'\
-        ' }'
-
-        dic = dict()
-        for row in g.query(query):
-            dic['id'] = str(row.asdict()['id'])
-            dic['municipio'] = str(row.asdict()['mun'])
-            dic['departamento'] = str(row.asdict()['dep'])
-            dic['servicio'] = str(row.asdict()['ser'])
-            dic['like'] = like
-        lista.append(dic)
-    
-    Likes = {"ips": lista}
-    listadep = get_departamentos()
-    listaMunicipips = get_municipios()
-    listaderv=["Hospitalaria","Consultas","Experiencia global","Urgencias"]
-
-    query ='PREFIX ips:<http://www.EPSColombia.org#>\
+    if len(data) == 0:
+        dic = []
+    else:
+        lista = []
+        for d in data:
+            idips = d[0]
+            like = d[1]
+            query ='PREFIX ips:<http://www.EPSColombia.org#>\
             SELECT *\
             WHERE {\
-                ?i ips:idips ?id;\
-                ips:ips ?nom;\
+                ?ips ips:idips ?id;\
                 ips:nomservicio ?ser;\
                 ips:municipio ?mun;\
                 ips:departamento ?dep\
-            }'
+                FILTER REGEX(?id, "' + idips + '+")'\
+            ' }'
 
-    data = []
-    for row in g.query(query):
-        data2 = dict()
-        data2['id'] = str(row.asdict()['id'])
-        data2['nombre'] = str(row.asdict()['nom'])
-        data2['servicio'] = str(row.asdict()['ser'])
-        data2['municipio'] = str(row.asdict()['mun'])
-        data2['departamento'] = str(row.asdict()['dep'])
-        data.append(data2)
-    ips = {'ips': data}
-
-    municipiosLike={}
-    municipiosNoLike={}
-    probLikeMuni={}
-    proNobLikeMuni={}
-    ProbTotalesMuni={}
-    for mun in listaMunicipips:
-        municipiosLike[mun]=0
-        municipiosNoLike[mun]=0
-        probLikeMuni[mun]=0
-        proNobLikeMuni[mun]=0
-        ProbTotalesMuni[mun]=0
-    
-        ## se llenan los diccioarios por de departameno
-        departamentosLike={}
-        departamentosNoLike={}
-        probLikeDepto={}
-        probNoLikeDepto={}
-        probTotalesDpto={} 
-    for dep in listadep:
-        departamentosLike[dep]=0
-        departamentosNoLike[dep]=0
-        probLikeDepto[dep]=0
-        probNoLikeDepto[dep]=0
-        probTotalesDpto[dep]=0
-    
-    serviciosLike={}
-    serviciosNoLike={}
-    probLikeServ={}
-    probNoLikeServ={}
-    probTotalesServ={}  
-    for servi in listaderv:
-        serviciosLike[servi]=0
-        serviciosNoLike[servi]=0
-        probLikeServ[servi]=0
-        probNoLikeServ[servi]=0
-        probTotalesServ[servi]=0
-    
-    probLike=0.5
-    recomendacion={}
-    Calificaciones=0
-    contLike=0
-    contNoLike=0
-    for i in Likes["ips"] :
-        Calificaciones = Calificaciones +1
+            dic = dict()
+            for row in g.query(query):
+                dic['id'] = str(row.asdict()['id'])
+                dic['municipio'] = str(row.asdict()['mun'])
+                dic['departamento'] = str(row.asdict()['dep'])
+                dic['servicio'] = str(row.asdict()['ser'])
+                dic['like'] = like
+            lista.append(dic)
         
-        if i["like"]==1 :
-            contLike=contLike+1
-            serviciosLike[i["servicio"]]=serviciosLike[i["servicio"]]+1
-            municipiosLike[i["municipio"]]=municipiosLike[i["municipio"]]+1
-            departamentosLike[i["departamento"]]=departamentosLike[i["departamento"]]+1
+        Likes = {"ips": lista}
+        listadep = get_departamentos()
+        listaMunicipips = get_municipios()
+        listaderv=["Hospitalaria","Consultas","Experiencia global","Urgencias"]
 
-        if i["like"]==0 :
-            contNoLike=contNoLike+1
-            serviciosNoLike[i["servicio"]]=serviciosNoLike[i["servicio"]]+1
-            municipiosNoLike[i["municipio"]]=municipiosNoLike[i["municipio"]]+1
-            departamentosNoLike[i["departamento"]]=departamentosNoLike[i["departamento"]]+1
-            
-
-    # saca las probabilidades condicionadas de like para cada feuture
-    for serv in probLikeServ.keys():
-        
-        if serviciosLike[serv]==0:
-            probLikeServ[serv]=0.001
-        else:
-            probLikeServ[serv]=serviciosLike[serv]/sum(serviciosLike.values())
-            
-            
-    for dep in probLikeDepto.keys():
-        
-        if departamentosLike[dep]==0:
-            probLikeDepto[dep]=0.001
-        else:
-            probLikeDepto[dep]=departamentosLike[dep]/sum(departamentosLike.values())
-
-    for muni in probLikeMuni.keys():
-        
-        if municipiosLike[muni]==0:
-            probLikeMuni[muni]=0.001
-        else:
-            probLikeMuni[muni]=municipiosLike[muni]/sum(municipiosLike.values())    
-            
-
-    # saca la probabilidadd condicionada de no like para cada atributo      
-        
-    for serv in probNoLikeServ.keys():
-        
-        if serviciosNoLike[serv]==0:
-            probNoLikeServ[serv]=0.001
-        else:
-            probNoLikeServ[serv]=serviciosNoLike[serv]/contNoLike
-            
-    for dep in probNoLikeDepto.keys():
-        
-        if departamentosNoLike[dep]==0:
-            probNoLikeDepto[dep]=0.001
-        else:
-            probNoLikeDepto[dep]=departamentosNoLike[dep]/contNoLike
-
-    for muni in proNobLikeMuni.keys():
-        
-        if municipiosNoLike[muni]==0:
-            proNobLikeMuni[muni]=0.001
-        else:
-            proNobLikeMuni[muni]=municipiosNoLike[muni]/contNoLike   
-            
-            
-    #calcular las probbilidades totales
-            
-            
-    for serv in probTotalesServ.keys():
-        if (serviciosLike[serv]+serviciosNoLike[serv])==0:
-            probTotalesServ[serv]=0.001
-        else:
-            probTotalesServ[serv]=(serviciosLike[serv]+serviciosNoLike[serv])/(sum(serviciosLike.values())+sum(serviciosNoLike.values()))
-            
-            
-    for dep in probTotalesDpto.keys():
-        if (departamentosLike[dep]+departamentosNoLike[dep])==0:
-            probTotalesDpto[dep]=0.001
-        else:
-            probTotalesDpto[dep]=(departamentosLike[dep]+departamentosNoLike[dep])/(sum(departamentosLike.values())+sum(departamentosNoLike.values()))
-
-
-    for muni in ProbTotalesMuni.keys():
-        if (municipiosLike[muni]+municipiosNoLike[muni])==0:
-            ProbTotalesMuni[muni]=0.001
-        else:
-            ProbTotalesMuni[muni]=(municipiosLike[muni]+municipiosNoLike[muni])/(sum(municipiosLike.values())+sum(municipiosNoLike.values()))
-            
-            
-    # se hace el calculo de probabilidad que le guste
-            
-    for ips in ips["ips"]:
-        ID=ips["id"]
-        servicio=ips["servicio"]
-        municipio=ips["municipio"]
-        departamento=ips["departamento"]    
-        prob=(probLike*probLikeServ[servicio]*probLikeDepto[departamento]*probLikeMuni[municipio])/(probTotalesServ[servicio]*probTotalesDpto[departamento]*ProbTotalesMuni[municipio])
-        
-        if ID in recomendacion:
-            if prob> recomendacion[ID]:
-                recomendacion[ID]=prob
-        else :
-            recomendacion[ID]=prob
-
-    sorrecomendacion=collections.OrderedDict(sorted(recomendacion.items()))
-    dic = []
-    for id in sorrecomendacion:
         query ='PREFIX ips:<http://www.EPSColombia.org#>\
                 SELECT *\
                 WHERE {\
-                    ?ips ips:idips ?id;\
+                    ?i ips:idips ?id;\
                     ips:ips ?nom;\
-                    FILTER REGEX(?id, "'+ id + '")\
-                 }'       
-        data = dict()
-        for row in g.query(query):
-            data['id'] = str(row.asdict()['id'])
-            data['nombre'] = str(row.asdict()['nom'])
-        dic.append(data)
-    
+                    ips:nomservicio ?ser;\
+                    ips:municipio ?mun;\
+                    ips:departamento ?dep\
+                }'
 
+        data = []
+        for row in g.query(query):
+            data2 = dict()
+            data2['id'] = str(row.asdict()['id'])
+            data2['nombre'] = str(row.asdict()['nom'])
+            data2['servicio'] = str(row.asdict()['ser'])
+            data2['municipio'] = str(row.asdict()['mun'])
+            data2['departamento'] = str(row.asdict()['dep'])
+            data.append(data2)
+        ips = {'ips': data}
+
+        municipiosLike={}
+        municipiosNoLike={}
+        probLikeMuni={}
+        proNobLikeMuni={}
+        ProbTotalesMuni={}
+        for mun in listaMunicipips:
+            municipiosLike[mun]=0
+            municipiosNoLike[mun]=0
+            probLikeMuni[mun]=0
+            proNobLikeMuni[mun]=0
+            ProbTotalesMuni[mun]=0
+        
+            ## se llenan los diccioarios por de departameno
+            departamentosLike={}
+            departamentosNoLike={}
+            probLikeDepto={}
+            probNoLikeDepto={}
+            probTotalesDpto={} 
+        for dep in listadep:
+            departamentosLike[dep]=0
+            departamentosNoLike[dep]=0
+            probLikeDepto[dep]=0
+            probNoLikeDepto[dep]=0
+            probTotalesDpto[dep]=0
+        
+        serviciosLike={}
+        serviciosNoLike={}
+        probLikeServ={}
+        probNoLikeServ={}
+        probTotalesServ={}  
+        for servi in listaderv:
+            serviciosLike[servi]=0
+            serviciosNoLike[servi]=0
+            probLikeServ[servi]=0
+            probNoLikeServ[servi]=0
+            probTotalesServ[servi]=0
+        
+        probLike=0.5
+        recomendacion={}
+        Calificaciones=0
+        contLike=0
+        contNoLike=0
+        for i in Likes["ips"] :
+            Calificaciones = Calificaciones +1
+            
+            if i["like"]==1 :
+                contLike=contLike+1
+                serviciosLike[i["servicio"]]=serviciosLike[i["servicio"]]+1
+                municipiosLike[i["municipio"]]=municipiosLike[i["municipio"]]+1
+                departamentosLike[i["departamento"]]=departamentosLike[i["departamento"]]+1
+
+            if i["like"]==0 :
+                contNoLike=contNoLike+1
+                serviciosNoLike[i["servicio"]]=serviciosNoLike[i["servicio"]]+1
+                municipiosNoLike[i["municipio"]]=municipiosNoLike[i["municipio"]]+1
+                departamentosNoLike[i["departamento"]]=departamentosNoLike[i["departamento"]]+1
+                
+
+        # saca las probabilidades condicionadas de like para cada feuture
+        for serv in probLikeServ.keys():
+            
+            if serviciosLike[serv]==0:
+                probLikeServ[serv]=0.001
+            else:
+                probLikeServ[serv]=serviciosLike[serv]/sum(serviciosLike.values())
+                
+                
+        for dep in probLikeDepto.keys():
+            
+            if departamentosLike[dep]==0:
+                probLikeDepto[dep]=0.001
+            else:
+                probLikeDepto[dep]=departamentosLike[dep]/sum(departamentosLike.values())
+
+        for muni in probLikeMuni.keys():
+            
+            if municipiosLike[muni]==0:
+                probLikeMuni[muni]=0.001
+            else:
+                probLikeMuni[muni]=municipiosLike[muni]/sum(municipiosLike.values())    
+                
+
+        # saca la probabilidadd condicionada de no like para cada atributo      
+            
+        for serv in probNoLikeServ.keys():
+            
+            if serviciosNoLike[serv]==0:
+                probNoLikeServ[serv]=0.001
+            else:
+                probNoLikeServ[serv]=serviciosNoLike[serv]/contNoLike
+                
+        for dep in probNoLikeDepto.keys():
+            
+            if departamentosNoLike[dep]==0:
+                probNoLikeDepto[dep]=0.001
+            else:
+                probNoLikeDepto[dep]=departamentosNoLike[dep]/contNoLike
+
+        for muni in proNobLikeMuni.keys():
+            
+            if municipiosNoLike[muni]==0:
+                proNobLikeMuni[muni]=0.001
+            else:
+                proNobLikeMuni[muni]=municipiosNoLike[muni]/contNoLike   
+                
+                
+        #calcular las probbilidades totales
+                
+                
+        for serv in probTotalesServ.keys():
+            if (serviciosLike[serv]+serviciosNoLike[serv])==0:
+                probTotalesServ[serv]=0.001
+            else:
+                probTotalesServ[serv]=(serviciosLike[serv]+serviciosNoLike[serv])/(sum(serviciosLike.values())+sum(serviciosNoLike.values()))
+                
+                
+        for dep in probTotalesDpto.keys():
+            if (departamentosLike[dep]+departamentosNoLike[dep])==0:
+                probTotalesDpto[dep]=0.001
+            else:
+                probTotalesDpto[dep]=(departamentosLike[dep]+departamentosNoLike[dep])/(sum(departamentosLike.values())+sum(departamentosNoLike.values()))
+
+
+        for muni in ProbTotalesMuni.keys():
+            if (municipiosLike[muni]+municipiosNoLike[muni])==0:
+                ProbTotalesMuni[muni]=0.001
+            else:
+                ProbTotalesMuni[muni]=(municipiosLike[muni]+municipiosNoLike[muni])/(sum(municipiosLike.values())+sum(municipiosNoLike.values()))
+                
+                
+        # se hace el calculo de probabilidad que le guste
+                
+        for ips in ips["ips"]:
+            ID=ips["id"]
+            servicio=ips["servicio"]
+            municipio=ips["municipio"]
+            departamento=ips["departamento"]    
+            prob=(probLike*probLikeServ[servicio]*probLikeDepto[departamento]*probLikeMuni[municipio])/(probTotalesServ[servicio]*probTotalesDpto[departamento]*ProbTotalesMuni[municipio])
+            
+            if ID in recomendacion:
+                if prob> recomendacion[ID]:
+                    recomendacion[ID]=prob
+            else :
+                recomendacion[ID]=prob
+
+        sorrecomendacion=collections.OrderedDict(sorted(recomendacion.items()))
+        dic = []
+        for id in sorrecomendacion:
+            query ='PREFIX ips:<http://www.EPSColombia.org#>\
+                    SELECT *\
+                    WHERE {\
+                        ?ips ips:idips ?id;\
+                        ips:ips ?nom;\
+                        FILTER REGEX(?id, "'+ id + '")\
+                    }'       
+            data = dict()
+            for row in g.query(query):
+                data['id'] = str(row.asdict()['id'])
+                data['nombre'] = str(row.asdict()['nom'])
+            dic.append(data)
+    
     response = app.response_class(
         response = json.dumps({'ips': dic}),
         status = 200,
